d8358cb57e924a9c989357d1de8cf174203ebe58
Juliopiress <juliopiressnts9@gmail.com>
2024-11-20 17:37:06 -0300

    Sebas estacionamento 20/11

--- Estacionamento/script.js
+++ Estacionamento/script.js
@@ -6,41 +6,55 @@ window.revelar = ScrollReveal({reset:true});
 
 revelar.reveal('.section1', {
     duration: 2000,
-    distance: '90px'
+    distance: '120px'
 })
 
 
 revelar.reveal('.section2', {
     duration: 2000,
-    distance: '120px'
-})
-
-revelar.reveal('.section3', {
-    duration: 2000,
-    distance: '200px'
+    distance: '120px',
+    origin: 'rigth'
 })
 
 revelar.reveal('.cabeçalho',{
-    duration: 1500,
-    distance : '60px'
+    duration: 2000,
+    distance : '120px'
 })
 
-revelar.reveal('.img-div',{
-    duration: 1500,
-    distance : '60px'
+revelar.reveal('.section3',{
+    duration: 2000,
+    distance : '120px'
 })
-//----------------------------------------------------------------------------------------------------------
 
-let currentIndex = 0;
+revelar.reveal('.conteudo-estacionamento',{
+    duration: 1800,
+    distance : '215px',
+    origin: 'rigth'
+})
 
-function changeSlide(direction) {
-    const slides = document.querySelectorAll('.slide');
-    const totalSlides = slides.length;
 
-    // Atualiza o índice atual
-    currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+//-----------------------------------------Codigo carrousel-----------------------------------------------
 
-    // Move o carrossel
-    const carousel = document.querySelector('.carousel');
-    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
+$(document).ready(function() {
+    //seleciona o container. carolsel e aplica a função slick()
+    //que ativa o carrossel com as configurações abaixo
+    $('.carousel').slick({
+        //slidesToshow: numero de cards exibidos ao mesmo tempo no carrossel
+        slidesToShow: 3,
+        //slidesto scroll: define quantos cards serao deslizados ao mover o carrossel
+        slidesToScroll: 1,
+        //infinit: quando true, o carrossel retorna ao inicio automaticamente
+        //mantem o carrossel em rotação continua
+        infinit: true, //mantem o carrossel em rotação continua
+
+        //dots: quando true, ativa setas de navegação nas laterais do carrolssel
+        dots: true,
+
+        //arrows: exibe setas de navegação nas laterais do carrosel
+        arrows: true,
+        //ativa a ssetas para navegar entre os cards
+
+
+    })
+})
+// ---------------------------------------------------------------------------------------------------------
