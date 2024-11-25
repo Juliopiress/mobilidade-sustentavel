8eb6ee8d61197c7a08f297ccb2428f0dcf5133ce
Daniela Varela <dannyvsouza@hotmail.com>
2024-11-25 00:46:54 -0300

    TCC mobilidade sustentavel
    TCC mobilidade sustentável 25/11

---
+++ Pagina01/scroll.js
@@ -0,0 +1,30 @@
+
+//Inicializando o efeito ScrollReveal no H2 na segunda seção (#section2)
+
+ScrollReveal().reveal("#sect2-h2", {
+    duration: 2000, // Duração do efeito (em milissegundos), define a velocidade da transição.
+    origin: 'left', // Origem do efeito: o conteúdo virá de baixo para cima
+    distance: '50px', // Distância que o elemento "viaja" antes de aparecer
+});
+//Efeito ScrollReveal no P na segunda seção (#section2)
+ScrollReveal().reveal("#sect2-p", {
+    duration: 2000, // Duração do efeito (em milissegundos), define a velocidade da transição.
+    origin: 'left', // Origem do efeito: o conteúdo virá de baixo para cima
+    distance: '50px', // Distância que o elemento "viaja" antes de aparecer
+});
+
+//Efeito ScrollReveal na imagem na segunda seção (#section2)
+ScrollReveal().reveal("#sect2-img", {
+    duration: 2000, // Duração do efeito (em milissegundos), define a velocidade da transição.
+    origin: 'right', // Origem do efeito: o conteúdo virá de baixo para cima
+    distance: '50px', // Distância que o elemento "viaja" antes de aparecer
+});
+
+
+//Inicializando o efeito ScrollReveal na terceira seção (#section3)
+
+ScrollReveal().reveal("#section3", {
+    duration: 4000, // Duração do efeito (em milissegundos), define a velocidade da transição.
+    origin: 'right', // Origem do efeito: o conteúdo virá de baixo para cima
+    distance: '50px', // Distância que o elemento "viaja" antes de aparecer
+});
